81126b416d871b43bb315c22d00a07029c9c5f71
Joca0805 <joaquim.silva45@etec.sp.gov.br>
2023-09-13 17:08:40 -0300

    new file:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var lista_de_produtos = []
+
+
+while(true){
+
+    var opcao = prompt(`
+    Bem vindo ao cadastro de produtos! :)
+    
+    1. Cadastrar novo produto
+    2. Editar produto
+    3. Remover produto
+    4. Viuualizar todos os produtos
+`)
+
+if(opcao == 1) {
+    //codigo para adicionar produto
+}else if(opcao == 2){
+    //codigo para editar produto
+}else if(opcao == 3){
+    //codigo para remover
+}else if(opcao == 4){
+    //codigo para visualizar
+}else{
+    alert("Nunhuma das opções foi selecionada!")
+}
+}
