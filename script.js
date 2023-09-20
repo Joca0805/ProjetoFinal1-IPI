68dab64e5e967ed6d03786f1e70b27ffbf1f7ce3
Joca0805 <144272448+Joca0805@users.noreply.github.com>
2023-09-20 17:33:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,18 +9,32 @@ while(true){
     1. Cadastrar novo produto
     2. Editar produto
     3. Remover produto
-    4. Viuualizar todos os produtos
+    4. Visualizar todos os produtos
 `)
 
 if(opcao == 1) {
-    //codigo para adicionar produto
+    var produto = Cadastrar()
+    lista_de_produtos.push(produto)
+
 }else if(opcao == 2){
     //codigo para editar produto
+    var i = prompt("Digite o indice do produto que deseja editar de 0 à " + (lista_de_produtos.length - 1));
+    var produtoEditar = prompt("Digite o novo nome do produto: " + lista_de_produtos[i]);
+    lista_de_produtos[i] = produtoEditar;
 }else if(opcao == 3){
     //codigo para remover
+    var i = prompt("Digite o indice do produto que deseja editar de 0 à " + (lista_de_produtos.length - 1));
+    lista_de_produtos.splice(i,1);
+
 }else if(opcao == 4){
     //codigo para visualizar
+    alert(lista_de_produtos.join("\n"));
 }else{
     alert("Nunhuma das opções foi selecionada!")
+}
+
+function Cadastrar() {
+    var produto = prompt("Digite o nome do produto");
+    return produto;
 }
 }
